c1ce5a3c0298d59476a0d4fb5c0b407d3c91b1e4
rudraranjan001 <125184056+rudraranjan001@users.noreply.github.com>
2025-06-15 15:26:38 +0000

    operater & operation

---
+++ suruwat/operation.js
@@ -0,0 +1,15 @@
+// console.log("1" + 2); // 12
+// console.log(2 + "1"); //21
+// console.log("2" + "1"); //21
+// console.log( "2" + 2 + "1"); //221
+// console.log("2" + 1 + 2); //212
+
+// console.log(1 + 3 + "3"); // 43
+//from above we understand that it convert it by scanning the first letter 
+//do not write these code it is very messy 
+// console.log(+""); // 0
+// console.log(+true); // 1
+let num = 5
+let n = 5
+console.log(++num); //pre increment -> increment first then display
+console.log(n++); // post increment -> display first then increment
