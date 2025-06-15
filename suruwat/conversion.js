154a22ffbce32d9c26eb38403ebb063eae15f478
rudraranjan001 <125184056+rudraranjan001@users.noreply.github.com>
2025-06-15 14:57:11 +0000

    conversion ka gyaan

---
+++ suruwat/conversion.js
@@ -0,0 +1,34 @@
+// let score = "45greg"
+
+// console.log(typeof score)
+
+// let num = Number(score) //explicit conversion
+// console.log(typeof num)
+// console.log(num) //NAN -> Not a Number 
+
+// let result = "5" * "4" //implicit conversion
+// console.log(result) //20 
+
+// let score = null
+
+// console.log(typeof score)
+
+// let num = Number(score) 
+// console.log(typeof num) //object
+// console.log(num) //0
+
+/*
+"33" => 33
+"33dsf" => NAN
+true => 1 , false => 0
+
+*/
+let isLoggedIn = ""
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//1 => true , 0-> false
+/*
+"" => false
+"ienv" => true
+*/ 
