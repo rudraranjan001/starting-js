89804e98ecb88cef3531b42eb2889b95290bf7ee
rudraranjan001 <125184056+rudraranjan001@users.noreply.github.com>
2025-06-15 14:31:33 +0000

    data type ka gyaan

---
+++ suruwat/data_types.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using node js not browser
+
+//code readibility should be high
+//Types of Variable
+/*
+number => range 2^53
+bigint
+string => ""
+boolean -> for true/false
+null -> standalone value/empty value
+undefined -> not assigned
+symbol -> uniqueness
+
+//object
+*/
+//yha pe hum ek word use karte hai saare variable ko declare karne ke lie
+let str="hiii"
+let num = 53423
+let valid = false
+console.log(typeof valid)
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof null) //object
+console.log(typeof undefined)  //undefined
+
--- suruwat/var.js
+++ suruwat/var.js
@@ -1,1 +1,15 @@
+const accountId = 144423
+let accountEmail = "rudrahhhf@gmai.com"
+var accountPassword = "12442"
+accountCity = "Muzaffarpur"
 
+// accountId = 234 //not allowed
+accountEmail = "hhheefef@gmail.com"
+accountPassword = "2342"
+accountCity = "hajipur"
+
+let accountstate
+console.log(accountEmail);
+console.log(accountCity);
+console.log(accountPassword);
+console.table([accountEmail,accountId,accountPassword,accountstate,accountCity])
