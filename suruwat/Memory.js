571186a4ce535fc05efb9c7b767a91cbdddbfd5c
rudraranjan001 <125184056+rudraranjan001@users.noreply.github.com>
2025-06-16 12:24:44 +0000

    stack&heap

---
+++ suruwat/Memory.js
@@ -0,0 +1,18 @@
+//Stack (primitive) , Heap (Non - Primitive)
+let myYoutubename = "codingbro"
+
+let anothername = myYoutubename
+
+anothername = "brocode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userone = {
+    email : "dveg@google.com",
+    upi: "dnrer@gtr"
+}
+
+let userTwo = userone
+
+// stack ke andar value ka copy provide hoti hai 
+// heap ke andar reference provide ki jaati hai
