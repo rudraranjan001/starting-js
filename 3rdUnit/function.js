dd24fc221ab753fb9ed477a1f21260bb21496103
rudraranjan001 <125184056+rudraranjan001@users.noreply.github.com>
2025-06-20 09:10:10 +0000

    kuchhh jankari function ki

--- 3rdUnit/function.js
+++ 3rdUnit/function.js
@@ -27,11 +27,43 @@ const result = add(4443523524,234345252352352324)
 function checkloggedin(username){
     return `${username} Just logged in`
 }
-console.log(checkloggedin("hellboy"));
-console.log(checkloggedin());//interview question if we dont pass any value then what will it display meanwhile its output is undefined Just logged in .this clears that no string pass means leads to the undefined name
+// console.log(checkloggedin("hellboy"));
+// console.log(checkloggedin());//interview question if we dont pass any value then what will it display meanwhile its output is undefined Just logged in .this clears that no string pass means leads to the undefined name
 
 //using default value
 function burbak(username = "H S Prasad"){
     return `${username} just logged in`
 }
-console.log(burbak("bhosdiyawala"));
+// console.log(burbak("bhosdiyawala"));
+
+//rest operator ->...
+//if we dont know how many arguments will come then we will use this trick
+function calculate(...num1){
+    return num1
+}
+// console.log(calculate(455,3523,545,56));
+
+//Interview Question
+function cal(val1,val2,...num3){
+    return num3
+}
+// console.log(cal(234,2356,12,6756,45));
+
+//function for object
+const user = {
+    username1 : "rudra",
+    id : "23525"
+}
+function handle(otherobject){
+    // console.log(`Username is ${otherobject.username1} \nid is ${otherobject.id}`);
+}
+handle(user)
+
+
+//function for object
+const arr = [2523,575,4344]
+
+function arrr(newarray){
+    return newarray[4]//if you demand the higher index which is not available it returns undefined
+}
+console.log(arrr(arr));
