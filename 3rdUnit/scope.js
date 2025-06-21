c21d7440c98fa26f292038aabdc55e5fe6bbb9c5
rudraranjan001 <125184056+rudraranjan001@users.noreply.github.com>
2025-06-21 08:08:19 +0000

    json and scopes

--- 3rdUnit/scope.js
+++ 3rdUnit/scope.js
@@ -6,7 +6,7 @@ if(true){ //scope
     var c = 66
 }
 // console.log(a);
-console.log(c); //var does obey the rule of local scope and global scope it always on global scope
+// console.log(c); //var does obey the rule of local scope and global scope it always on global scope
 // console.log(b);
 
 //nested scope
@@ -17,5 +17,36 @@ function ooe(){
         const web1 = "tumse milna"
         console.log(name)
     }
-    console.log(web1)
+    // console.log(web1)
+    // twii()
 }
+// ooe()
+
+//+++++++++++Interesting++++++++
+console.log(addone(44))
+function addone(num){
+    return num+1
+}
+
+console.log(addtwo(5))// it gives errror because it stores in variable it is discussed in hosting    
+const addtwo = function(num){ // also known as expression
+    return num+2
+}
+//In js variable can hold anything like json value ,function ,etc...
+
+
+//Json -> It stands for Javascript Object Notation.It is a lightweight data format that is used to store data and exchange the data.Especially for client and a server.
+/*
+this is Json
+{
+  "name": "Rudra",
+  "age": 22,
+  "isStudent": true,
+  "skills": ["C++", "JavaScript", "Python"],
+  "address": {
+    "city": "Delhi",
+    "zip": "110001"
+  }
+}
+It is used in Web APIs
+*/
