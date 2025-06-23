bf1d674e3fe1c010067252b191149ef1b6897645
rudraranjan001 <125184056+rudraranjan001@users.noreply.github.com>
2025-06-23 07:22:08 +0000

    for of loop

---
+++ Loops/for_of.js
@@ -0,0 +1,27 @@
+//for of loop
+//It is array specific loop
+ 
+//javascipt property of loops
+
+// const arr = [342,5235,5323,23523,55555]
+// for (const index of arr) {
+//      console.log(index);
+// }
+
+//Maps -> it a special data structure  that helps to store keys and value in pairs . just like cpp.but it contain elements as compared to cpp
+const map = new Map()
+map.set('in',"India")
+map.set('un',"United Nation")
+map.set('usa',"United state of america")
+map.set('aus',"Australia")
+map.set('aus',"Australia")
+
+// console.log(map);
+ 
+// for (const [key,value] of map) {
+//     console.log(key,` = `,value);
+    
+// }
+
+//loop in object
+//object can't be iterable using for of object
